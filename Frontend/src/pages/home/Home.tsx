3e34a28f9b6c4afc97721d4d4f424d122f598533
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-01-29 20:36:19 +0300

    creation de la page home

---
+++ Frontend/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react/jsx-runtime";
+import Headerfirstpage from "../../components/headerfirstpage/Headerfirstpage";
+import Herofirstpage from "../../components/herofirstpage/Herofirstpage";
+import Footerfirstpage from "../../components/footerfirstpage/Footerfirstpage";
+
+const Home: React.FC<{}> = () => {
+  return (
+    <Fragment>
+     
+    </Fragment>
+  );
+};
+
+export default Home;
